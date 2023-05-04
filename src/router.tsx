56a0d6f7719a5602920236a350ad4c2ebb37906d
xuyanliang <1137599122@qq.com>
2023-05-04 18:11:57 +0800

    路由懒加载

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,20 @@
 import { RouteObject } from 'react-router-dom'
 
 import Home from './pages/home'
-import Details from './pages/details'
+import { lazy } from 'react'
+
+const sleep = (time: number) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('')
+        }, time)
+    })
+}
+
+const Details = lazy(async () => {
+    await sleep(5000)
+    return import('./pages/details/')
+})
 
 const routers: RouteObject[] = [
     {
@@ -10,7 +23,9 @@ const routers: RouteObject[] = [
     },
     {
         path: '/details',
-        element: <Details />,
+        // element: <Details />,
+        // element:()=>Details,
+        Component: Details,
         children: [
             {
                 index: true,
